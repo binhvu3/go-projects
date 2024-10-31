cfc12b0154e0316644232fa3eec63036d2295b0e
binhvu3 <binhhtvu@gmail.com>
2024-10-31 14:25:58 -0700

    added update api to mongodb

--- to-do/main.go
+++ to-do/main.go
@@ -9,14 +9,15 @@ import (
 	"github.com/gofiber/fiber/v2"
 	"github.com/joho/godotenv"
 	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
 type Todo struct {
-	Id        int    `json:"_id"  bson:"_id"`
-	Completed bool   `json:"_completed"  bson:"_completed"`
-	Body      string `json:"_body"  bson:"_body"`
+	ID        primitive.ObjectID `json:"id, omitempty"  bson:"_id,omitempty"`
+	Completed bool               `json:"completed"  bson:"completed"`
+	Body      string             `json:"body"  bson:"body"`
 }
 
 var collection *mongo.Collection
@@ -52,7 +53,7 @@ func main() {
 	app := fiber.New()
 	app.Get("/api/todos", getTodos)
 	app.Post("/api/todos", createTodo)
-	// app.Patch("/api/todos/:id", updateTodo)
+	app.Patch("/api/todos/:id", updateTodo)
 	// app.Delete("/api/todos/:id", deleteTodo)
 
 	port := os.Getenv("PORT")
@@ -86,13 +87,47 @@ func getTodos(c *fiber.Ctx) error {
 }
 
 func createTodo(c *fiber.Ctx) error {
-	
+	todo := new(Todo)
 
+	if err := c.BodyParser(todo); err != nil {
+		return err
+	}
+
+	if todo.Body == "" {
+		return c.Status(400).JSON(fiber.Map{"error": "Todo body cannot be empty!"})
+	}
+
+	insertResult, err := collection.InsertOne(context.Background(), todo)
+
+	if err != nil {
+		return err
+	}
+
+	todo.ID = insertResult.InsertedID.(primitive.ObjectID)
+
+	return c.Status(200).JSON(todo)
 }
 
-// func updateTodo(c *fiber.Ctx) error {
+func updateTodo(c *fiber.Ctx) error {
+	id := c.Params("id")
+	objectID, err := primitive.ObjectIDFromHex(id)
 
-// }
+	if err != nil {
+		return c.Status(404).JSON(fiber.Map{"error": "Object Not Found!"})
+	}
+
+	filter := bson.M{"_id": objectID}
+	update := bson.M{"$set": bson.M{"completed": true}}
+
+	_, err = collection.UpdateOne(context.Background(), filter, update)
+
+	if err != nil {
+		return err
+	}
+
+	return c.Status(200).JSON(fiber.Map{"status": "success"})
+
+}
 
 // func deleteTodo(c *fiber.Ctx) error {
 
