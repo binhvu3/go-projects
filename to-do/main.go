fc4fc937cc5ae2c269cd8fb9dd44df942c6d1ea2
binhvu3 <binhhtvu@gmail.com>
2024-10-31 15:14:21 -0700

    added delete api to mongodb

--- to-do/main.go
+++ to-do/main.go
@@ -54,7 +54,7 @@ func main() {
 	app.Get("/api/todos", getTodos)
 	app.Post("/api/todos", createTodo)
 	app.Patch("/api/todos/:id", updateTodo)
-	// app.Delete("/api/todos/:id", deleteTodo)
+	app.Delete("/api/todos/:id", deleteTodo)
 
 	port := os.Getenv("PORT")
 	if port == "" {
@@ -129,6 +129,22 @@ func updateTodo(c *fiber.Ctx) error {
 
 }
 
-// func deleteTodo(c *fiber.Ctx) error {
+func deleteTodo(c *fiber.Ctx) error {
+	id := c.Params("id")
+	objectID, err := primitive.ObjectIDFromHex(id)
 
-// }
+	if err != nil {
+		return c.Status(404).JSON(fiber.Map{"Error": "Object Not Found!"})
+	}
+
+	filter := bson.M{"_id": objectID}
+
+	_, err = collection.DeleteOne(context.Background(), filter)
+
+	if err != nil {
+		return err
+	}
+
+	return c.Status(200).JSON(fiber.Map{"Status": "Deleted Sucessfuly"})
+
+}
